86b6a0d8c6ccc75a1cc844a7b7189d81283f3cb7
Marcus Carvalho <marcuswac@gmail.com>
2016-07-06 15:12:49 -0300

    Fixing problem when downloading data. Ignoring certificate verification

--- scripts/download_and_extract_input_data.sh
+++ scripts/download_and_extract_input_data.sh
@@ -1,2 +1,2 @@
-wget http://www.lsd.ufcg.edu.br/~marcus/cloudrm-sim/cloudrm-input-data-google.tar.gz
+wget --no-check-certificate http://www.lsd.ufcg.edu.br/~marcus/cloudrm-sim/cloudrm-input-data-google.tar.gz
 tar xvzf cloudrm-input-data-google.tar.gz && rm cloudrm-input-data-google.tar.gz
