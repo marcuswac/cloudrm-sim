bb031bed742764a5411b901d907c97166e4cc3d7
Marcus Carvalho <marcuswac@gmail.com>
2016-07-06 22:20:32 -0300

    Update install_R_packages.R

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,3 +1,3 @@
 install.packages(c("dplyr", "RSQLite", "foreach", "forecast", "ggplot2", "queueing", "tidyr",
                    "scales", "RColorBrewer", "gridExtra"),
-                   repos = "https://cloud.r-project.org/")
+                 repos = "https://cloud.r-project.org/")
