d3ec8bace47fef5b194667e0adab5ea6536433b3
Marcus Carvalho <marcuswac@gmail.com>
2016-07-26 14:54:32 -0300

    Updating simulation execution scripts

--- scripts/run_admission_control_sim.sh
+++ scripts/run_admission_control_sim.sh
@@ -1,35 +1,62 @@
 #!/bin/bash
+# 
+# Usage:
+# ./src/admission_control_sim.R [--] [--help] [--consider-mem] [--opts opts] [--cpu-capacity-factor factor] \
+#                               [--mem-capacity-factor factor] [--cpu-load-factor factor] [--mem-load-factor factor] \
+#                               [--slo-scenario scenario] [--output-file-prefix prefix] method
+# Input parameters for admission control simulation:
 #
-# Input arguments:
-# - method: name of the admission control method (greedy-norejection, greedy-quota,
-#   forecast-mean-quota, forecast-ets-quota)
-# - cpu_capacity_factor: decimal factor applied to the original cloud CPU capacity. A factor = 1 simulates
-#   the cloud with the same CPU capacity found in the traces.
-# - cpu_load_factor: decimal factor applied to the original cloud CPU load. A factor = 1 simulates
-#   the cloud with the same CPU load (requested resources) found in the traces.
-# - slo_scenario: integer that identifies the availability SLO scenario. Possible values:
-#   - 1: medium
-#   - 2: very low
-#   - 3: low
-#   - 4: high
-#   - 5: very high
+# mandatory arguments:
 #
-# - mem_capacity_factor: decimal factor applied to the original cloud memory capacity. A factor = 1 simulates
-#   the cloud with the same memory capacity found in the traces.
-# - mem_considered: string ("yes" or "no") defining if memory is considered in admission control
-#   decisions.
-# - mem_load_factor: decimal factor applied to the original cloud Memory load. A factor = 1 simulates
-#   the cloud with the same Memory load (requested resources) found in the traces.
+#  method                    Name of the admission control method.
+#                            Options: <greedy-norejection, greedy-quota, forecast-mean-quota, forecast-ets-quota>
 #
-# Sample run:
-# scripts/run_admission_control_sim.sh "forecast-ets-quota" 1 1 1 1 "yes" 1
+# flags:
+#
+#  -h, --help                          Show this help message and exit
+#
+#  -cmem, --consider-mem               Flag that defines if memory is considered in admission control decisions.
+#                                      [default: FALSE]
+#
+# optional arguments:
+#
+#  -x, --opts OPTS                     RDS file containing argument values
+#
+#  -ccf, --cpu-capacity-factor FACTOR  Decimal factor applied to the original cloud CPU capacity.
+#                                      A factor = 1 simulates the cloud with the same CPU capacity found in the
+#                                      traces. [default: 1]
+#
+#  -mcf, --mem-capacity-factor FACTOR  Decimal factor applied to the original cloud memory capacity.
+#                                      A factor = 1 simulates the cloud with the same memory capacity found in
+#                                      the traces. [default: 1]
+#
+#  -clf, --cpu-load-factor FACTOR      Decimal factor applied to the original cloud CPU load.
+#                                      A factor = 1 simulates the cloud with the same CPU load
+#                                      (requested resources) found in the traces. [default: 1]
+#
+#  -mlf, --mem-load-factor FACTOR      Decimal factor applied to the original cloud Memory load.
+#                                      A factor = 1 simulates the cloud with the same Memory load
+#                                      (requested resources)found in the traces. [default: 1]
+#
+#  -s, --slo-scenario SCENARIO         Integer that identifies the availability SLO scenario. Possible values:
+#                                      1 (medium); 2 (very low); 3 (low); 4 (high); 5 (very high). [default: 1]
+#
+#  -o, --output-file-prefix PREFIX     Prefix for the CSV output file names resulted from simulations.
+#                                      [default: "res"]
+#
+#
+# Sample execution of a base scenario:
+#
+# ./src/admission_control_sim.R forecast-ets-quota --cpu-capacity-factor 1 --mem-capacity-factor 1 --cpu-load-factor 1 \
+#                            --mem-load-factor 1 --slo-scenario 1 --consider-mem
 #
 # For details, see paper:
 # Prediction-Based Admission Control for IaaS Clouds with Multiple Service. CloudCom'2015.
 # 
+###################################################################################################
 
 ARGS=$*
 
-SCRIPT_FILE="src/probabilistic_admission_control.R"
+SCRIPT_FILE="src/admission_control_sim.R"
 
 Rscript $SCRIPT_FILE $ARGS  
