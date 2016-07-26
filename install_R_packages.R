d7eb8b7f729735ec21b0fd374111a2689f5f39e1
Marcus Carvalho <marcuswac@gmail.com>
2016-07-26 14:28:40 -0300

    Updating R package installation script

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+# Usage: ./install_R_packages.R
+
 install.packages(c("dplyr", "RSQLite", "foreach", "forecast", "ggplot2", "queueing", "tidyr",
-                   "scales", "RColorBrewer", "gridExtra", "argparser", "readr"),
+                   "scales", "RColorBrewer", "gridExtra", "argparser"),
                  repos = "https://cloud.r-project.org/")
