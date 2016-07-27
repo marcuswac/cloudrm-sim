1193f2ce754f26b1813c26c966a4c87bb8f3e9d1
Marcus Carvalho <marcuswac@gmail.com>
2016-07-26 21:16:33 -0300

    Fixing R package installation and updating SQLite dependency

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 # Usage: ./install_R_packages.R
 
+lib_dir <- Sys.getenv("R_LIBS_USER")
+dir.create(lib_dir, showWarnings = T, recursive = T)
+
 install.packages(c("dplyr", "RSQLite", "foreach", "forecast", "ggplot2", "queueing", "tidyr",
                    "scales", "RColorBrewer", "gridExtra", "argparser"),
-                 repos = "https://cloud.r-project.org/")
+                 lib = lib_dir, repos = "https://cloud.r-project.org/")
