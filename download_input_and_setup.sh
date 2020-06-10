7bf85c64adcea0e47d94a9c045afeaec94d0cdca
Marcus Carvalho <marcuswac@gmail.com>
2020-06-10 12:08:01 -0300

    changing input data download link

--- download_input_and_setup.sh
+++ download_input_and_setup.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate http://www.lsd.ufcg.edu.br/~marcus/cloudrm-sim/cloudrm-input-data-google.tar.gz
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1wmp3rMwjEoP1u2V08TupeaZcy3dgf4Ur' -O cloudrm-input-data-google.tar.gz
 tar xvzf cloudrm-input-data-google.tar.gz && rm cloudrm-input-data-google.tar.gz
 mkdir output 2> /dev/null
 mkdir plots 2> /dev/null
